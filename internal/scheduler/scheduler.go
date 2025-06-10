4bd226a1549df43880394c87019a6722314e1a47
Tian <tian@stellarsol.info>
2025-06-10 17:41:44 +0800

    feat(scheduler): 完善定時搜尋功能，新增可用場地通知邏輯
    在 SchedulerService 中新增 Telegram Bot 介面，實作場地可用性檢查及通知使用者的功能。新增排序邏輯以依照星期及時段排列訂閱，並在有可用場地時透過 Telegram 發送通知。此更新提升了使用者體驗，確保即時通知可預約場地的狀態。

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -2,9 +2,13 @@ package scheduler
 
 import (
 	"context"
+	"fmt"
+	"sort"
+	"strconv"
 	"sync"
 	"time"
 
+	tgbot "github.com/tian841224/crawler_sportcenter/internal/bot/tg_bot"
 	"github.com/tian841224/crawler_sportcenter/internal/crawler"
 	"github.com/tian841224/crawler_sportcenter/internal/domain/schedule"
 	"github.com/tian841224/crawler_sportcenter/pkg/logger"
@@ -14,6 +18,7 @@ import (
 type SchedulerService struct {
 	nantunSportCenter crawler.NantunSportCenterBotInterface
 	schedule          schedule.Service
+	tgBot             tgbot.TGBotInterface
 	mutex             sync.RWMutex
 	stopChan          chan struct{}
 }
@@ -25,9 +30,10 @@ type SchedulerInterface interface {
 
 var _ SchedulerInterface = (*SchedulerService)(nil)
 
-func NewSchedulerService(nantunSportCenter crawler.NantunSportCenterBotInterface, schedule schedule.Service) *SchedulerService {
+func NewSchedulerService(nantunSportCenter crawler.NantunSportCenterBotInterface, schedule schedule.Service, tgBot tgbot.TGBotInterface) *SchedulerService {
 	return &SchedulerService{
 		nantunSportCenter: nantunSportCenter,
+		tgBot:             tgBot,
 		schedule:          schedule,
 		stopChan:          make(chan struct{}),
 	}
@@ -65,16 +71,50 @@ func (s *SchedulerService) checkAllSubscriptions(ctx context.Context) error {
 		return err
 	}
 
-	for chatID, subs := range *scheduleList {
-		// TODO:
-		// 排序時段
-		// 檢查是否有可用場地
-		// 如果有可用場地，通知使用者
+	// 按照星期排序
+	sort.Slice(*scheduleList, func(i, j int) bool {
+		return (*scheduleList)[i].Weekday < (*scheduleList)[j].Weekday
+	})
 
+	// 按照時段排序
+	sort.Slice(*scheduleList, func(i, j int) bool {
+		return (*scheduleList)[i].TimeSlot.StartTime.Before((*scheduleList)[j].TimeSlot.StartTime)
+	})
+
+	currentWeekday := time.Now().Weekday()
+	currentTime := time.Now().Hour()
+	availableTimeSlotsLength := 0
+
+	for chatID, subs := range *scheduleList {
+		// 查詢的時間一樣則直接通知使用者
+		if subs.Weekday != currentWeekday || subs.TimeSlot.StartTime.Hour() != currentTime {
+			// 檢查是否有可用場地
+			var err error
+			availableTimeSlots, err := s.nantunSportCenter.GetAvailableTimeSlots(subs.Weekday.String(), subs.TimeSlot.StartTime.Hour(), strconv.Itoa(chatID))
+			if err != nil {
+				logger.Log.Error("checkAllSubscriptions", zap.Error(err))
+				continue
+			}
+			availableTimeSlotsLength = len(availableTimeSlots)
+		}
+
+		// 沒有可用場地直接跳過
+		if availableTimeSlotsLength == 0 {
+			continue
+		}
+
+		// 如果有可用場地，通知使用者
+		message := fmt.Sprintf("星期 %s 時段 %d:00-%d:00 有可用場地",
+			subs.Weekday,
+			subs.TimeSlot.StartTime.Hour(),
+			subs.TimeSlot.EndTime.Hour())
+
+		s.tgBot.SendMessage(int64(chatID), message)
+
+		currentWeekday = subs.Weekday
+		currentTime = subs.TimeSlot.StartTime.Hour()
 		logger.Log.Debug("checkAllSubscriptions", zap.Int("chatID", chatID), zap.Any("subs", subs))
 	}
 
 	return nil
 }
-
-// TODO: 通知使用者 - 使用介面而非直接呼叫 bot
