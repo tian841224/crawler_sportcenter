f046b8cf09a18a3d3260ab4199f5cadbce90ae59
Tian <jacky841224j@gmail.com>
2025-06-17 01:07:56 +0800

    feat(user): 新增運動中心帳號與密碼欄位至使用者模型
    在使用者模型中新增了運動中心帳號及密碼欄位，以支援運動中心相關功能的整合，提升系統的擴展性與靈活性。

--- internal/domain/user/model.go
+++ internal/domain/user/model.go
@@ -3,11 +3,13 @@ package user
 import "time"
 
 type User struct {
-	ID        uint      `gorm:"primaryKey;column:id;autoIncrement"`
-	AccountID string    `gorm:"column:account_id;type:varchar(50);unique;not null"`
-	Status    bool      `gorm:"column:status;not null"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoUpdateTime"`
+	ID                  uint      `gorm:"primaryKey;column:id;autoIncrement"`
+	AccountID           string    `gorm:"column:account_id;type:varchar(50);unique;not null"`
+	Status              bool      `gorm:"column:status;not null"`
+	SportCenterAccount  string    `gorm:"column:sport_center_account;type:varchar(50)"`
+	SportCenterPassword string    `gorm:"column:sport_center_password;type:varchar(50)"`
+	CreatedAt           time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt           time.Time `gorm:"column:updated_at;autoUpdateTime"`
 }
 
 func (User) TableName() string {
